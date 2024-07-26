1d8c3c371bffa8ae635aa6134b5cda4564316184
Blissmal <malutibethuel@gmail.com>
2024-07-26 15:34:25 +0300

    updated cardDeal

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,6 +1,20 @@
+import styles, {layout} from "../style.js";
+import Button from "./Button.jsx";
+
 const CardDeal = () => {
     return (
-        <div></div>
+        <section className={layout.section}>
+            <div className={layout.sectionInfo}>
+                <h2 className={styles.heading2}>
+                    Find a better card deal <br className="sm:block hidden"/> in a few easy steps.
+                </h2>
+                <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid aut error expedita optio? Ducimus laborum minus molestias officiis perspiciatis.
+                </p>
+                <Button styles="mt-10"/>
+            </div>
+
+        </section>
     )
 }
 export default CardDeal;
