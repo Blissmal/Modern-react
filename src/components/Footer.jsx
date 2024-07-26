60b89bdb9959f4b8810bca31b6a13dce3f9c4558
Blissmal <malutibethuel@gmail.com>
2024-07-26 17:56:39 +0300

    complete Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import styles from "../style.js";
 import {logo} from "../assets/index.js";
-import {footerLinks} from "../constants/index.js";
+import {footerLinks, socialMedia} from "../constants/index.js";
 
 const Footer = () => {
     return (
@@ -31,8 +31,16 @@ const Footer = () => {
                 </div>
             </div>
 
-            <div>
+            <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
                 <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">2021 HooBank. All Rights Reserved.</p>
+
+                <div className="flex flex-row md:mt-0 mt-6">
+                    {
+                        socialMedia.map((social, index) => (
+                            <img key={social.id} src={social.icon} alt="" className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length - 1 ? 'mr-6' : 'mr-0'}`} />
+                        ))
+                    }
+                </div>
             </div>
         </section>
     )
