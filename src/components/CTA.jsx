7bed7c3c4ca6f7d30b9945cdf36d06b1b30f76fe
Blissmal <malutibethuel@gmail.com>
2024-07-26 16:24:53 +0300

    updated CTA

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,6 +1,17 @@
+import Button from "./Button.jsx";
+import styles from "../style.js";
+
 const CTA = () => {
     return (
-        <div></div>
+        <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
+            <div>
+                <h2>Let&apos;s try our services now!</h2>
+                <p>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
+            </div>
+            <div>
+                <Button />
+            </div>
+        </section>
     )
 }
 export default CTA;
