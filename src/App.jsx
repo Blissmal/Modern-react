aacf1d205d98bd8d24b6afd6535ac5f5a42c5a81
Blissmal <malutibethuel@gmail.com>
2024-07-26 10:59:57 +0300

    modern reactjs

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import styles from "./style.js";
+
+const App = () => {
+    return (
+        <div className="bg-primary w-full overflow-hidden">
+            <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+                <div className={`${styles.boxWidth}`}>
+                    Navbar
+                </div>
+            </div>
+
+
+            <div className={`bg-primary ${styles.flexStart}`}>
+                <div className={`${styles.boxWidth}`}>
+                    Hero
+                </div>
+            </div>
+
+        </div>
+    )
+}
+export default App
